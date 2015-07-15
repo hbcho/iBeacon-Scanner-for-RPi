9276081cb0d5632128d18a9ac939932109dbbcb8
Nicolas Roos <nicolas.roos@serranetga.com>
2015-07-15 14:43:24 +0200

    minor change

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import bluetooth._bluetooth as bluez
 
 dev_id = 0
 try:
-    sock = bluez.hci_open_dev(dev_id)
-    print "ble thread started"
+	sock = bluez.hci_open_dev(dev_id)
+	print "ble thread started"
 
 except:
-    print "error accessing bluetooth device..."
-    sys.exit(1)
+	print "error accessing bluetooth device..."
+	sys.exit(1)
 
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
@@ -21,5 +21,5 @@ blescan.hci_enable_le_scan(sock)
 returnedList = blescan.parse_events(sock, 10)
 
 print "----------"
-for beacon in returnedList:
-    print beacon
+for beac in returnedList:
+	print beac
