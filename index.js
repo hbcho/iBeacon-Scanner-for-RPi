8e05f4ccdf299b94316bd7b9e68fae4fe7e640ac
Nicolas Roos <nicolas.roos@serranetga.com>
2015-07-21 16:40:52 +0200

    improved quite a bit

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.get('/',function(req,res) {
 	res.sendfile('index.html');
 });
 
-app.get('/beacons.json',function(req,res) {
-	res.setHeader('Content-Type','application/json');
-	res.sendfile('beacons.json');
+app.get('/v1/beacons',function(req,res) {
+	fs.readFile('beacons.json',function(err, data) {
+		res.setHeader('Content-Type','application/json');
+		if(err || data.length === 0) {
+			 res.send({});
+		} else {
+			res.send(data);
+		}
+	});
 });
 
 var port = process.env.PORT || 5000;
