a41a89187009e58e90adf5e7c305b6861cc9c340
Nicolas Roos <nicolas.roos@serranetga.com>
2015-07-21 10:32:38 +0200

    improved beacon page

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-	res.sendfile('index.html');
+	res.sendFile('index.html');
 });
 
 app.get('/beacons.json',function(req,res) {
 	res.setHeader('Content-Type','application/json');
-	res.sendfile('beacons.json');
+	res.sendFile('beacons.json');
 });
 
 var port = process.env.PORT || 5000;
