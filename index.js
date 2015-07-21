6a9c206be5c33e531ec7ea7646f66d4960014db7
Nicolas Roos <nicolas.roos@serranetga.com>
2015-07-21 11:03:56 +0200

    catching fetch error

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-	res.sendFile('index.html');
+	res.sendfile('index.html');
 });
 
 app.get('/beacons.json',function(req,res) {
 	res.setHeader('Content-Type','application/json');
-	res.sendFile('beacons.json');
+	res.sendfile('beacons.json');
 });
 
 var port = process.env.PORT || 5000;
