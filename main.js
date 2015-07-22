36937377dfaf93378c79fda870e53be376039a50
Nicolas Roos <nicolas.roos@serranetga.com>
2015-07-22 14:30:11 +0200

    fixed typos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var child_process = require('child_process');
 
-child_process.exec('sudo ibeacon_can -b',function(error,stdout,stderr) {
+child_process.exec('sudo ./ibeacon_scan -b',function(error,stdout,stderr) {
 	console.log('stdout:' + stdout);
 	console.log('stderr:' + stderr);
 	if(error !== null) {
